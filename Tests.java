c7bbe73b8ec723e53e531a752a9b622404dc304d
nnaeyno <ngogo20@freeuni.edu.ge>
2023-04-30 16:35:17 +0400

    second test passed

--- Tests.java
+++ Tests.java
@@ -21,4 +21,18 @@ public class Tests {
         assertEquals(1200.0, data.getProfit(PROD_ID001), 0);
 
     }
+
+    @Test
+    public void testTwo(){
+        Data data = new Data();
+        data.saveProduct(PROD_ID001, PROD_NAME, 2800);
+        data.purchaseProduct(PROD_ID001, 5, 2500);
+        data.purchaseProduct(PROD_ID001, 5, 3000);
+        assertEquals(10, data.getQuantity(PROD_ID001));
+        assertEquals(2750, data.getAverage(PROD_ID001),  0);
+        data.orderProduct(PROD_ID001, 4);
+        assertEquals(data.getQuantity(PROD_ID001), 6);
+        assertEquals(200, data.getProfit(PROD_ID001), 0);
+
+    }
 }
