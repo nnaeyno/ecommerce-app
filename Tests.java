9482fd884b43b7d2c53b6693b0d6b20f15c21f2f
nnaeyno <ngogo20@freeuni.edu.ge>
2023-04-30 16:30:53 +0400

    first test passed

---
+++ Tests.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class Tests {
+    final String PROD_ID001 = "prod001";
+    final String PROD_ID002 = "prod002";
+    final String PROD_NAME = "iphone";
+
+    @Test
+    public void testOne(){
+        Data data = new Data();
+        data.saveProduct(PROD_ID001, PROD_NAME, 2800);
+        assertEquals(0, data.getQuantity(PROD_ID001));
+        data.purchaseProduct(PROD_ID001, 5, 2500);
+        assertEquals(5, data.getQuantity(PROD_ID001));
+        assertEquals(2500, data.getAverage(PROD_ID001),  0);
+        data.orderProduct(PROD_ID001, 4);
+        assertEquals(data.getQuantity(PROD_ID001), 1);
+        assertEquals(1200.0, data.getProfit(PROD_ID001), 0);
+
+    }
+}
